26a66df82f46df57d601e4c43f6b021eb28d706c
urturn <gmlakd5632@gmail.com>
2020-04-28 15:08:36 +0900

    사용자 예약표 발급 화면 형태만 만든 것

---
+++ app/src/main/java/com/example/number_ticket/CsRsvActivity.java
@@ -0,0 +1,14 @@
+package com.example.number_ticket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CsRsvActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cs_rsv);
+    }
+}
