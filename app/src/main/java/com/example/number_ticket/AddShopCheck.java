a3857585e69faf0bf99a614864ca887293620929
urturn <gmlakd5632@gmail.com>
2020-05-05 19:01:42 +0900

    각종 팝업창 만들었음. intent 연결때문에 필요할까봐 만들자마자 올림. 최종본x

---
+++ app/src/main/java/com/example/number_ticket/AddShopCheck.java
@@ -0,0 +1,12 @@
+package com.example.number_ticket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddShopCheck extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_shop_check);
+    }
+}
