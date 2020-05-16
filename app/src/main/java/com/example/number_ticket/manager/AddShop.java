744dc42727acf030e67088770dc0029b29b1527a
faulty337 <faulty337@gmail.com>
2020-05-16 21:42:12 +0900

    에러수정

--- app/src/main/java/com/example/number_ticket/manager/AddShop.java
+++ app/src/main/java/com/example/number_ticket/manager/AddShop.java
@@ -3,6 +3,7 @@ package com.example.number_ticket.manager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
